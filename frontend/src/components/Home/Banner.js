92cebc3920d6ac268797f1fda13f3b4f9d3adffe
Yawer Ijaz <yawerijaz@gmail.com>
2022-07-22 01:52:46 -0700

    add search bar component and wirings

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,15 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import SearchBar from "./SearchBar";
 
-const Banner = () => {
+const Banner = (props) => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <SearchBar onSearchBoxUpdate={props.onSearchBoxUpdate}></SearchBar>
           <span> the cool stuff.</span>
         </div>
       </div>
---
+++ frontend/src/components/Home/SearchBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import agent from "../../agent";
+
+
+const SearchBar = (props) => {
+
+    function handleSearchTermChange(ev) {
+        let term = ev.target.value;
+        if (term.length >= 3) {
+            props.onSearchBoxUpdate(
+                term, 
+                (page) => agent.Items.byTitleSearch(term, page), 
+                agent.Items.byTitleSearch(term)
+            );    
+        }
+    }
+
+    return (
+        <span className="product-search-form has-search">
+            props.onSearchBoxUpdate
+            <input 
+                type="text" 
+                placeholder="What is it that you truly desire?"
+                value={props.searchTerm}
+                onChange={handleSearchTermChange}
+            ></input>
+        </span>
+    );
+}
+
+export default SearchBar;
