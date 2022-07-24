c9bc5db3a9e3469d1296e46815a6559397cc4e2d
Yawer Ijaz <yawerijaz@gmail.com>
2022-07-24 02:08:19 -0700

    seed 100 fake

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+100.times do |index|
+    User.create!(username: "user#{index}", email: "email#{index}@foo", password: '**********')
+end
+
+p "Created #{User.count} users"
+
+Item.destroy_all
+
+100.times do |index|
+    Item.create!(title: "item#{index}", description: "description#{index}", user: User.where(username: "user#{index}").first())
+end
+
+p "Created #{Item.count} items"
+
+Comment.destroy_all
+
+100.times do |index|
+    Comment.create!(body: "hi", user: User.where(username: "user#{index}").first(), item: Item.where(title: "item#{index}").first())
+end
+
+p "Created #{Comment.count} comments"
+
+
